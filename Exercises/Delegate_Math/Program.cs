4dd02ff8358beab99874caf45c4a260891795e28
Sindhuja Swamidorai <sindhuja-swamidorai@pluralsight.com>
2024-07-24 19:06:15 +0530

    Exercise files

---
+++ Exercises/Delegate_Math/Program.cs
@@ -0,0 +1,49 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Math;
+
+Console.WriteLine("Hello, World!");
+
+BasicMath math = new BasicMath();
+MathOperation mathOp = new MathOperation(math.Add);
+
+Console.WriteLine(mathOp(1, 2));
+
+mathOp = new MathOperation(math.Subtract);
+
+Console.WriteLine(mathOp(1, 2));
+
+mathOp = new MathOperation(math.Multiply);
+
+Console.WriteLine(mathOp(1, 2));
+
+mathOp = new MathOperation(math.Divide);
+
+Console.WriteLine(mathOp(1, 2));
+
+delegate double MathOperation(double a, double b);
+
+namespace Math
+{
+    class BasicMath
+    {
+        public double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        public double Subtract(double a, double b)
+        {
+            return a - b;
+        }
+
+        public double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+        public double Divide(double a, double b)
+        {
+            return a / b;
+        }
+
+    }
+}
---
+++ Exercises/LINQandObjects/Program.cs
@@ -0,0 +1,91 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Dynamic;
+using System.Reflection;
+using People;
+
+Console.WriteLine("Hello, World!");
+
+List<Person> people = GetPeople();
+
+//GetMalesUnder25(people);
+AveregeAgePerGender(people);
+
+
+void GetMalesUnder25(List<Person> people)
+{
+    List<Person> result = (from p in people where p.Gender == "Male" && p.Age < 25 select p).ToList();
+
+    Console.WriteLine("List of males under 25:");
+    foreach (Person p in result)
+    {
+        p.DisplayPerson();
+    }
+}
+
+void AveregeAgePerGender(List<Person> people)
+{
+    List<IGrouping <string,int>> result ;
+
+    result = (from p in people group p.Age by p.Gender).ToList();
+
+    Console.WriteLine("Average age of males and females in the group:");
+
+    for(int i=0; i < result.Count; i++)
+    {
+        Console.WriteLine($"{result[i].Key}: {result[i].Average()}");
+    }
+}
+
+
+static List<Person> GetPeople()
+{
+    List<Person> persons = new List<Person>();
+
+    for (int i = 0; i < 20; i++)
+    {
+        Person singlePerson = new Person();
+        singlePerson.Name = $"Name{i}";
+        singlePerson.Age = new Random().Next(10, 60);
+        singlePerson.Gender = (i % 3 == 0) ? "Male" : "Female";
+        singlePerson.DisplayPerson();
+        persons.Add(singlePerson);
+    }
+
+    return persons;
+}
+
+
+namespace People
+{
+    public class Person
+    {
+        private string name;
+        private string gender;
+        private int age;
+        private string homeTown;
+
+        public string Name { get { return name; } set { name = value; } }
+        public string Gender { get { return gender; } set { gender = value; } }
+        public int Age { get { return age; } set { age = value; } }
+
+        public string HomeTown { get { return homeTown; } set { homeTown = value; } }
+
+        public Person()
+        {
+            name = "xyz";
+            gender = "male";
+            age = 60;
+            homeTown = "Bengaluru";
+        }
+
+        public void DisplayPerson()
+        {
+                Console.WriteLine(name);
+                Console.WriteLine(gender);
+                Console.WriteLine(age);
+                Console.WriteLine(homeTown);
+        }
+
+    }
+
+}
